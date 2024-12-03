1f8247d9e22c0eb0ba1b29379b37c7cc1b65bd49
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-12-03 18:03:10 +1100

    Refactor part 1 processing code to be reusable in part 2

--- 2024/day3/index.js
+++ 2024/day3/index.js
@@ -5,15 +5,26 @@ function part1() {
   const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
   const matches = input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
 
+  const sum = processMatches(matches);
+  console.log(`Sum: ${sum}`);
+}
+
+/**
+ * Find the sum of multiplying each pair in the list of matches
+ * @param {RegExpStringIterator<RegExpExecArray>} matches iterator for regex matches
+ * @returns 
+ */
+function processMatches(matches) {
   let sum = 0;
+
   for (const match of matches) {
-    const x = parseInt(match[1], 10);
-    const y = parseInt(match[2], 10);
+    const x = parseInt(match[1]);
+    const y = parseInt(match[2]);
     
     sum += x * y;
   }
 
-  console.log(`Sum: ${sum}`);
+  return sum
 }
 
 part1();
