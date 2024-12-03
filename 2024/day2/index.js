8adf5a35a7050aff2aa18cc4a109d8221d93aeb6
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-12-03 17:31:26 +1100

    Day 2 part 2 - unfinished

--- 2024/day2/index.js
+++ 2024/day2/index.js
@@ -63,4 +63,66 @@ function isReportSafe(report) {
   return true;
 }
 
+// While a lot of the part 2 code is the same as the part 1 code, it has been kept separate to demonstrate the difference between both problems/solutions.
+function part2() {
+  const input = fs.readFileSync(path.resolve(__dirname, 'temp.txt')).toString();
+  const reports = parseInput(input);
+
+  let safeReports = 0;
+  reports.forEach(report => {
+    if (isReportSafeWithProblemDampener(report, true)) {
+      safeReports++;
+    }
+  });
+
+  console.log(`${safeReports} safe reports`);
+}
+
+/**
+ * Recursively checks whether the report is safe with a tolerance of one fault
+ * @param {number[][]} report
+ * @param {boolean} canRetry True if checks can be retried by removing one element
+ * @returns Boolean indicating whether report is safe or not
+ */
+function isReportSafeWithProblemDampener(report, canRetry) {
+  const prevIncreasing = report[0] - report[1] < 0;
+
+  for (let i = 0; i < report.length; i++) {
+    let diff = report[i] - report[i+1];
+    if (i === report.length-1) {
+      diff = report[i-1] - report[i];
+    }
+
+    const absDiff = Math.abs(diff);
+    const increasing = diff < 0;
+
+    if (isDiffUnsafe(absDiff) || increasing !== prevIncreasing) {
+      const tempReport = report.slice();
+
+      if (canRetry) {
+        tempReport.splice(i, 1);
+
+        const isRetrySafe = isReportSafeWithProblemDampener(tempReport, false);
+
+        if (isRetrySafe) {
+          return true;
+        }
+
+        if (!isRetrySafe && i === report.length-2) {
+          continue;
+        }
+      }
+
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isDiffUnsafe(diff) {
+  return diff > 3 || diff < 1;
+}
+
 part1();
+part2();
