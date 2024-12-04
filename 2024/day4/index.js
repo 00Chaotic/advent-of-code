1549cb4c3ed7077bfed0d0e0f302d55214f88dab
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-12-04 22:22:44 +1100

    Day 4 part 2

--- 2024/day4/index.js
+++ 2024/day4/index.js
@@ -127,4 +127,53 @@ function findXmasMatches(matrix, y, x) {
   return count;
 }
 
+function part2() {
+  const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
+  const matrix = parseInput(input);
+
+  let count = 0;
+
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      const currentLetter = matrix[y][x];
+
+      if (currentLetter !== 'A') {
+        continue;
+      }
+
+      // Create smaller matrix with only relevant chars
+      const tempMatrix = [];
+      tempMatrix.push([matrix[y-1]?.[x-1], matrix[y-1]?.[x+1]]);
+      tempMatrix.push([matrix[y+1]?.[x-1], matrix[y+1]?.[x+1]]);
+
+      if (isHorizontalMas(tempMatrix, y, x)) {
+        count++;
+      }
+    }
+  }
+
+  console.log(`Count: ${count}`);
+}
+
+/**
+ * Processes an 'A' index in a 2x2 matrix to check whether an X-MAS pattern
+ * is found
+ * @param {char[][]} matrix At most 2x2
+ * @returns 
+ */
+function isHorizontalMas(matrix, y, x) {
+  return (
+    (
+      (matrix[0]?.[0] === 'M' && matrix[1]?.[1] === 'S') ||
+      (matrix[0]?.[0] === 'S' && matrix[1]?.[1] === 'M')
+    )
+    &&
+    (
+      (matrix[1]?.[0] === 'M' && matrix[0]?.[1] === 'S') ||
+      (matrix[1]?.[0] === 'S' && matrix[0]?.[1] === 'M')
+    )
+  )
+}
+
 part1();
+part2();
