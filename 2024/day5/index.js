48ada6c502e365f5eb7ee6f2d133ee1ab18ea886
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-12-05 22:39:25 +1100

    Day 5 part 1

---
+++ 2024/day5/index.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const path = require('path');
+
+// Custom object for easier data handling
+function OrderRule(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function part1() {
+  const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
+  const lines = input.split('\n');
+  
+  const separator = lines.findIndex(line => line.trim() === '');
+  const ruleStr = lines.slice(0, separator);
+  const updateStr = lines.slice(separator+1);
+
+  const rules = parseRules(ruleStr);
+  const updates = parseUpdates(updateStr);
+
+  const valid = validUpdates(rules, updates);
+
+  let sum = 0;
+  valid.forEach(row => {
+    const middleIndex = (row.length-1)/2;
+    sum += row[middleIndex];
+  });
+
+  console.log(`Sum of middle numbers of valid updates: ${sum}`);
+}
+
+/**
+ * 
+ * @param {string[]} input
+ * @returns {OrderRule[]} Array of rules sorted by ascending y value 
+ */
+function parseRules(input) {
+  const rules = input.map(line => {
+    const parts = line.split('|');
+    return new OrderRule(parseInt(parts[0]), parseInt(parts[1]));
+  });
+
+  // Sort by y to make future lookups a little quicker
+  rules.sort((a, b) => a.y - b.y);
+
+  return rules;
+}
+
+/**
+ * 
+ * @param {string[]} input
+ * @returns {number[][]} 2d array of update page numbers
+ */
+function parseUpdates(input) {
+  const updates = [];
+
+  input.forEach(line => {
+    const row = [];
+    const nums = line.trim().split(',');
+    
+    nums.forEach(num => {
+      row.push(parseInt(num));
+    });
+
+    updates.push(row);
+  });
+
+  return updates;
+}
+
+/**
+ * Evaluate a list of updates according to the specified list of rules
+ * @param {OrderRule[]} rules 
+ * @param {number[][]} updates
+ * @returns {number[][]}
+ */
+function validUpdates(rules, updates) {
+  const validUpdates = [];
+
+  for (i = 0; i < updates.length; i++) {
+    let invalidNums = [];
+    let isValid = true;
+
+    for (j = 0; j < updates[i].length; j++) {
+      const value = updates[i][j];
+
+      if (invalidNums.some(rule => rule.x === value)) {
+        isValid = false;
+        break;
+      }
+
+      invalidNums = invalidNums.concat(rules.filter(rule => !invalidNums.includes(value) && rule.y === value));
+    }
+
+    if (isValid) {
+      validUpdates.push(updates[i]);
+    }
+  }
+
+  return validUpdates;
+}
+
+part1();
